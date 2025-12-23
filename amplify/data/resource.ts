67430200d11239d6c7261f033e5318e527d895c2
gaoyuanbin <gaoyuanbin0305@gmail.com>
2025-12-23 21:17:56 +0800

    made sign in ui appear back. does not work on sandbox yet.

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -8,22 +8,20 @@ specifies that any user authenticated via an API key can "create", "read",
 =========================================================================*/
 const schema = a.schema({
   Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+      .model({
+        content: a.string(),
+        isDone: a.boolean(),
+      })
+      .authorization((allow) => [allow.owner()]),
 });
+
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    defaultAuthorizationMode: "userPool",
   },
 });
 
